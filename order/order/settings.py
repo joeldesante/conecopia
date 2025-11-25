ab1849c02a67b201bc1cc7bff98a5c00a8eee2cc
Joel DeSante <joeldesante@gmail.com>
2025-11-24 20:14:56 -0500

    Update settings

--- order/order/settings.py
+++ order/order/settings.py
@@ -27,6 +27,17 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
+CORS_ALLOWED_ORIGINS = [
+    "https://cnecopia.trysous.co",
+    "https://conecopia.com"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://conecopia.trysous.co",
+    "https://conecopia.com",
+]
+
+
 INSTALLED_APPS = [
     'index',
     'catalogue',
